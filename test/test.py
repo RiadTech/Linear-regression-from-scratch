574d2b134faf093e3f150c9c50f89a517ec17e7f
Riad <98049700+RiadTech@users.noreply.github.com>
2022-08-12 23:07:14 +1300

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,10 +1,21 @@
-from src.main import *
 import matplotlib.pyplot as plt
+from src.main import *
 import pandas as pd
+data = pd.read_csv("Your csv file")
 m = 0
 b = 0
 L = 0.0001
 epochs = 1000
 
 for i in range(epochs):
+    if i % 50 == 0:
+        print("Epoch: {i}")
+
+    
     m, b = gradient_descent(m, b)
+
+print(m, b)
+
+plt.scatter(data.studytime, data.score, color="red")
+plt.plot(list(range(20, 80)), (m *x + b for x in range(20, 80)), color="black")
+plt.show()
